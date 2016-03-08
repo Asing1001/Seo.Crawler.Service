e379be28aa7848f4213fe4ca9291f6b479b03b5e
josepht <mmx362003@gmail.com>
2016-03-08 23:54:37 +0800

    save test.

--- Seo.Crawler.Service/Crawler.cs
+++ Seo.Crawler.Service/Crawler.cs
@@ -10,34 +10,28 @@ using NLog;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
+using System.Threading.Tasks;
 
 namespace Seo.Crawler.Service
 {
     public class Crawler
     {
-        private RemoteWebDriver _driver;
+        //private RemoteWebDriver _driver;
         private CrawlerOptions _options;
-        private HashSet<Uri> pagesVisited;
-        private List<Uri> pagesToVisit;
         private Stopwatch _watch;
         private Logger logger;
-
         private static ConcurrentDictionary<Uri, Uri> pageParentURLMapping; // Key is current Page,Content is parent Page
-        private static ConcurrentBag<Uri> pagesToVisit;
+
+        private static ConcurrentDictionary<Uri, Uri> pagesToVisit;
 
         public Crawler(CrawlerOptions options)
         {
             _options = options;
             pageParentURLMapping = new ConcurrentDictionary<Uri, Uri>();
-            pagesToVisit = new ConcurrentBag<Uri>();
+            pagesToVisit = new ConcurrentDictionary<Uri,Uri>();
             _watch = new Stopwatch();
             logger = LogManager.GetCurrentClassLogger();
-            FirefoxProfile profile = new FirefoxProfile();
-            profile.SetPreference("general.useragent.override", _options.UserAgent);
-            _driver = new FirefoxDriver(profile);
-            //var chromeOptions = new ChromeOptions();
-            //chromeOptions.AddArgument("--user-agent=" + _options.UserAgent);
-            //_driver = new ChromeDriver(chromeOptions);
+            
         }
 
 
@@ -49,39 +43,60 @@ namespace Seo.Crawler.Service
 
         private void Crawl(Uri uri)
         {
-            pagesVisited.Add(uri);
-            logger.Info("[{0}] Open page :{1}", pagesVisited.Count, uri);
-            _driver.Navigate().GoToUrl(uri);
-            SaveHtmlAndScreenShot(uri);
-            pagesToVisit.AddRange(GetUnvisitedLinks());
-            if (pagesToVisit.Count >= 1 && pagesVisited.Count < _options.MaxPageToVisit)
+            
+            //First PAge
+            if (pageParentURLMapping.TryAdd(uri,null))
             {
-                Crawl(PopUrlFromPagesToVisit());
-            }
-            else
-            {
-                Finish();
-            }
 
+                var driver = new RemoteWebDriver(new Uri("http://localhost:4500/wd/hub"), DesiredCapabilities.Chrome()); // instead of this url you can put the url of your remote hub
+                driver.Navigate().GoToUrl(uri);
+                SaveHtmlAndScreenShot(uri, driver);
+                GetUnvisitedLinks(driver ,uri);
+
+
+                logger.Info("[{0}] Open page :{1}", pagesToVisit.Count, uri);
+                Parallel.ForEach(pagesToVisit, (keyValue) =>
+                {
+                    
+                    var _driver = new RemoteWebDriver(new Uri("http://localhost:4500/wd/hub"), DesiredCapabilities.Chrome()); // instead of this url you can put the url of your remote hub
+                    _driver.Navigate().GoToUrl(keyValue.Key);
+                    
+                    SaveHtmlAndScreenShot(uri, _driver);
+
+                    GetUnvisitedLinks(_driver, keyValue.Value);
+                    _driver.Quit();
+                    Uri value ;
+                    if (pageParentURLMapping.TryRemove(keyValue.Key,out value))
+                    {
+                        pageParentURLMapping.TryAdd(keyValue.Key, keyValue.Value);
+                    }
+
+                });
+            }
+            
+            
+
+            Finish();
         }
+
+
+        
+
+
 
         private void Finish()
         {
-            _driver.Dispose();
-            SaveSitemap();
+            
+            //SaveSitemap();
             _watch.Stop();
             logger.Info("Finish all task in {0}", _watch.Elapsed);
         }
 
-        private Uri PopUrlFromPagesToVisit()
-        {
-            var result = pagesToVisit.FirstOrDefault();
-            pagesToVisit.RemoveAt(0);
-            return result;
-        }
+       
 
-        private IEnumerable<Uri> GetUnvisitedLinks()
+        private void GetUnvisitedLinks(RemoteWebDriver _driver ,Uri parentUri)
         {
+            var result = new List<Uri>();
             var originHost = _options.StartUrl.Host;
             var links = _driver.FindElementsByCssSelector("a[href]")
                 .Select(a =>
@@ -96,12 +111,59 @@ namespace Seo.Crawler.Service
                     }
                 }
                 );
-            var sameDomainUnvisitedLinks = links.Where(link => link != null && link.Host.Contains(originHost) && !pagesVisited.Contains(link)
-                && !pagesToVisit.Contains(link));
-            return sameDomainUnvisitedLinks;
+
+            foreach (var link in links)
+            {
+                if (link != null && link.Host.Contains(originHost) && !pageParentURLMapping.ContainsKey(link)
+                    && !pagesToVisit.ContainsKey(link) && !result.Contains(link))
+                {
+                    result.Add(link );
+                }
+            }
+
+            foreach(var r in result)
+            {
+                pagesToVisit.TryAdd(r,parentUri);
+
+            }
+
+            logger.Info("Get {0} sameDomainUnvisitedLinks, list as below :{1}", result.Count
+                //JsonConvert.SerializeObject(result.Select(uri => uri.AbsolutePath))
+                );
+            //return result;
+
         }
 
-        private void SaveHtmlAndScreenShot(Uri uri)
+        private void ValidatePage(RemoteWebDriver _driver ,Uri currentUri)
+        {
+
+           /* DataRow drRow = pagesErrorList.NewRow();
+            drRow["URL"] = currentUri.AbsolutePath;
+            List<LogEntry> logEntry = _driver.Manage().Logs.GetLog(LogType.Browser).ToList();
+            Boolean ValidateFailed = false;
+
+            if (_driver.PageSource.Contains("Error 404") || _driver.PageSource.Contains("404") || _driver.PageSource.ToLower().Contains("not found"))
+            {
+                drRow["NotFound"] = " Page Not Found";
+                ValidateFailed = true;
+            }
+            if (logEntry.Count > 0)
+            {
+
+                drRow["LogCount"] = logEntry.Count.ToString();
+                drRow["Error"] += string.Join(" , ", logEntry.Where(log => !log.Message.Contains("$modal is now deprecated. Use $uibModal instead.")).Select(log => log.Message).ToList());
+                ValidateFailed = true;
+            }
+            if (ValidateFailed)
+            {
+                drRow["SourceURL"] = pageParentURLMapping.ContainsKey(currentUri.AbsolutePath) ? pageParentURLMapping[currentUri.AbsolutePath] : "";
+                pagesErrorList.Rows.Add(drRow);
+            }
+            if (pageParentURLMapping.Count > 0)
+                pageParentURLMapping.Remove(currentUri.AbsoluteUri);*/
+        }
+
+        private void SaveHtmlAndScreenShot(Uri uri, RemoteWebDriver _driver)
         {
             try
             {
@@ -135,7 +197,7 @@ namespace Seo.Crawler.Service
                 using (var fileStream = new FileStream(sitemapPath, FileMode.Create))
                 {
                     var siteMapGenerator = new SiteMapGenerator(fileStream, Encoding.UTF8);
-                    siteMapGenerator.Generate(pagesVisited);
+                    //siteMapGenerator.Generate(pagesVisited);
                     siteMapGenerator.Close();
                 }
                 logger.Info("SiteMap save to {0}", sitemapPath);
